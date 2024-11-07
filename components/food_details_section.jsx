f159b2c31e87e383de17fb008ecb6209ee339a84
John Omeyo <johnomeyo@gmail.com>
2024-11-07 13:03:09 +0300

    Created the food details card

--- components/food_details_section.jsx
+++ components/food_details_section.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView, Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 const FoodDetailsSection = () => {
@@ -7,17 +7,38 @@ const FoodDetailsSection = () => {
         <View>
             <Text style={styles.name}>Double Beef Cheese Burger</Text>
             <View style={styles.location}>
-                <Ionicons name="location" size={17} color="grey" onPress={() => console.log("Heart Pressed")} />
+                <Ionicons name="location" size={17} color="grey" onPress={() => console.log("Added to cart")} />
                 <Text style={styles.locationText}>Naperville, Illinois</Text>
             </View>
+            <View style={styles.foodInfo}>
+                <View style={styles.info}>
+                    <Text style={styles.figures}>198</Text>
+                    <Text style={styles.units}>kcal</Text>
+                </View>
+                <View style={styles.line} />
+                <View style={styles.info}>
+                    <Text style={styles.figures}>25.2</Text>
+                    <Text style={styles.units}>proteins</Text>
+                </View>
+                <View style={styles.line} />
+                <View style={styles.info}>
+                    <Text style={styles.figures}>13.8</Text>
+                    <Text style={styles.units}>fats</Text>
+                </View>
+                <View style={styles.line} />
+                <View style={styles.info}>
+                    <Text style={styles.figures}>23.7</Text>
+                    <Text style={styles.units}>carbo</Text>
+                </View>
+            </View>
+
             <Text style={styles.description}>Description</Text>
-            <Text style={styles.descriptionText}>Burger with patty filled with macaroni $ cheese and real beef, Stroganoff sauce, topped with onions, served on a soft black sesame burger bun</Text>
+            <Text style={styles.descriptionText}>Burger with patty filled with macaroni & cheese and real beef, Stroganoff sauce, topped with onions, served on a soft black sesame burger bun</Text>
 
             <View style={styles.buy}>
                 <View style={styles.iconCircle}>
                     <Ionicons name="bag-outline" size={24} color="grey" onPress={() => console.log("Heart Pressed")} />
                 </View>
-                {/* <Button title='Buy Now' color="green" ></Button> */}
                 <View style={styles.btn}>
                     <Text style={styles.btnText}>BUY NOW</Text>
                 </View>
@@ -31,6 +52,10 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
     },
+    line: {
+        height: '60%', backgroundColor: 'grey', // Line color
+        width: 1,
+    },
     location: {
         flexDirection: 'row',
         marginVertical: 10,
@@ -39,6 +64,16 @@ const styles = StyleSheet.create({
         color: 'grey',
         marginLeft: 10,
         fontSize: 17,
+    },
+    foodInfo: {
+        height: 80,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        padding: 12,
+        marginVertical: 10,
+        borderRadius: 12
     },
     description: {
         fontSize: 22,
@@ -78,6 +113,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: 200,
         borderRadius: 18
+    },
+    figures: {
+        fontWeight: '700',
+    },
+    units: {
+        color: 'grey'
     }
 });
 
