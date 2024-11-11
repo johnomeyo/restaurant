c999920b2908d0bd03e7a47236ba2aa61888797d
John Omeyo <johnomeyo@gmail.com>
2024-11-12 01:19:03 +0300

    created the card list tile

--- app/cart_page.jsx
+++ app/cart_page.jsx
@@ -9,7 +9,10 @@ const CartPage = () => {
     return (
         <View>
             <Text style={styles.title}>Cart Page</Text>
-            <CartListTile/>
+            <CartListTile name="Wooden" img='https://img.icons8.com/?size=100&id=kLCDEKFEmsPz&format=png&color=000000' color="Golden" price="450" />
+            <CartListTile name="Bakley" img='https://img.icons8.com/?size=100&id=108242&format=png&color=000000' color="Black" price="200" />
+            <CartListTile name="Rocking" img='https://img.icons8.com/?size=100&id=7USAgXD1WqTc&format=png&color=000000' color="Blue" price="200" />
+
         </View>
     )
 
@@ -19,7 +22,7 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 20,
         fontWeight: 'bold',
-        marginBottom:20,
+        marginBottom: 20,
     }
 })
 export default CartPage
--- components/cart_list_tile.jsx
+++ components/cart_list_tile.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-const CartListTile = () => {
+const CartListTile = ( props) => {
     return (
         <View style={styles.listTile}>
-            <View style={styles.imgCont}></View>
-            <View style={styles.details}></View>
-            <View style={styles.price}></View>
+            <Image
+                style={styles.image}
+                source={{ uri:props.img }} />
+            <View style={styles.details}>
+                <Text style={styles.category}>{props.color} Color</Text>
+                <Text style={styles.name}>{props.name}</Text>
+                <Text style={styles.price}>${props.price}</Text>
+            </View>
+            <View style={styles.items}>
+                <TouchableOpacity onPress={() => console.log("Reduced")}>
+                    <View style={styles.outline}>
+                        <Text>-</Text>
+                    </View>
+                </TouchableOpacity>
+                <Text>1</Text>
+                <TouchableOpacity onPress={() => console.log("Added")}>
+                    <View style={styles.outline}>
+                        <Text>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
@@ -15,6 +33,10 @@ const styles = StyleSheet.create({
         marginHorizontal: 16,
         flexDirection: "row",
         justifyContent: 'space-between',
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 12,
+        marginVertical: 8
 
     },
     imgCont: {
@@ -24,14 +46,43 @@ const styles = StyleSheet.create({
         borderRadius: 8
     },
     details: {
-        backgroundColor: "green",
         height: 100,
         width: 150,
+        justifyContent: 'space-between',
+        paddingVertical: 10
     },
-    price: {
-        backgroundColor: "blue",
+    items: {
         height: 100,
         width: 100,
-    }
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    image: {
+        width: 100,
+        height: 100,
+        alignSelf: 'center',
+    },
+    outline: {
+        height: 30,
+        width: 30,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#bdbdbd',
+    },
+    category: {
+        color: 'orange',
+        fontWeight: 'bold',
+        fontSize: 10,
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 22,
+    },
+    price: {
+        fontWeight: 'bold',
+        color: 'grey'
+    },
 })
 export default CartListTile;
